dc29d6e9013610ad2604186d8c777426cbd272c4
Priyanka-99 <priyanka99dhiman@gmail.com>
2020-09-25 15:11:23 +0530

    Update url_status.js

--- url_status.js
+++ url_status.js
@@ -1,13 +1,18 @@
+//import puppeteer libraries
 const puppeteer = require("puppeteer");
 
 async function run() {
+  //launch browser in headless mode
   const browser = await puppeteer.launch();
+  //page will be opened
   const page = await browser.newPage();
+	//checl for job portals' search page url
 	const mainUrl = "https://domdomjobs.com/search"
 	let mainUrlStatus;
   await page.setRequestInterception(true);
   page.on("request", request => {
     const url = request.url();
+    //display all requested urls present in js code 
     console.log("request url:", url);
     request.continue();
   });
@@ -19,15 +24,19 @@ async function run() {
     const request = response.request();
     const url = request.url();
     const status = response.status();
+    //display all urls with response status
     console.log("response url:", url, "status:", status);
 		if (url === mainUrl) {
 			mainUrlStatus = status;
 		}
   });
   await page.goto(mainUrl);
+	//display the status of the page url
 	console.log("status for main url:", mainUrlStatus);
   const html = await page.content();
+  //browser will get closed
   await browser.close();
 }
 
+//calling run function
 run();
